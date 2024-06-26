d2a97a17e40134dbfe69fb380309b63dcf6b685b
j218shir <j520218x@mails.cc.ehime-u.ac.jp>
2024-06-26 10:07:59 +0900

    Update a.php

--- a.php
+++ a.php
@@ -2,5 +2,7 @@
 <?php 
   foreach(file('/usr/share/dict/words') as $line){
     // strpos() 関数を使って、文字列 "xy" が含まれる単語を echo "$line"; で表示しなさい。
+    if (strpos($line, 'xy') !== false) {//文字列にxyが入っているかどうか
+      echo "$line";
   } 
 ?>
